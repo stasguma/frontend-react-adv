af281e5d7b6ac785fb047be092b9dc519633fdfb
Stas Guma <mcguma123@gmail.com>
2024-03-09 11:43:27 +0200

    add a Sidebar component

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,16 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 
 import { Sidebar } from '@/widgets/Sidebar';
+import { renderWithRouter } from '@/shared/lib/tests/withRouter';
 
 describe('<Sidebar />', () => {
   test('render', () => {
-    render(<Sidebar />);
+    renderWithRouter(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
 
   test('test a toggle function', () => {
-    render(<Sidebar />);
+    renderWithRouter(<Sidebar />);
     const toggleBtn = screen.getByTestId('sidebar-toggle');
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     fireEvent.click(toggleBtn);
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,11 +1,17 @@
 import { type FC, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+
+import { classNames } from '@/shared/lib/helpers/classNames/classNames';
+import { RoutePath } from '@/shared/config/routerConfig/routerConfig';
+import { Button, LangSwitcher } from '@/shared/ui';
+import { ThemeSwitcher } from '@/entities/ThemeSwitcher';
 
 import classes from './Sidebar.module.scss';
 
-import { classNames } from '@/shared/lib/helpers/classNames/classNames';
-import { Button, LangSwitcher } from '@/shared/ui';
-import { ThemeSwitcher } from '@/entities/ThemeSwitcher';
+import HomeIcon from '@/shared/assets/icons/home.svg';
+import CofeeIcon from '@/shared/assets/icons/coffee.svg';
+import MenuIcon from '@/shared/assets/icons/menu.svg';
 
 interface IProps {
   className?: string;
@@ -25,11 +31,53 @@ export const Sidebar: FC<IProps> = (props) => {
     <div
       data-testid="sidebar"
       className={classNames(classes.sidebar, className, {
-        [classes['is-collapsed']]: isCollapsed,
+        'is-collapsed': isCollapsed,
       })}
     >
-      <Button data-testid="sidebar-toggle" onClick={toggleSidebar}>X</Button>
-      <div className={classes.sidebar__content}>{t('sidebar')}</div>
+      <div className={classes.sidebar__header}>
+        <Button
+          data-testid="sidebar-toggle"
+          className={classes['sidebar__toggle-btn']}
+          onClick={toggleSidebar}
+        >
+          <MenuIcon />
+        </Button>
+      </div>
+      <div className={classes.sidebar__content}>
+        <nav
+          className={classes.navigation}
+          role="navigation"
+          aria-label="sidebar navigation"
+        >
+          <ul className={classes['navigation-list']} role="list">
+            <li className={classes.navigation__item} role="listitem">
+              <NavLink
+                className={({ isActive }) =>
+                  classNames(classes['navigation-link'], { 'is-active': isActive })}
+                to={RoutePath.home}
+                role="link"
+              >
+                <HomeIcon
+                  className={classNames(classes['navigation-link__icon'])}
+                />
+                <span className={classes['navigation-link__text']}>{t('home')}</span>
+              </NavLink>
+            </li>
+            <li className={classes.navigation__item} role="listitem">
+              <NavLink
+                className={({ isActive }) =>
+                  classNames(classes['navigation-link'], { 'is-active': isActive })}
+                to={RoutePath.about}
+              >
+                <CofeeIcon
+                  className={classNames(classes['navigation-link__icon'])}
+                />
+                <span className={classes['navigation-link__text']}>{t('about')}</span>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <div className={classes.sidebar__footer}>
         <ThemeSwitcher />
         <LangSwitcher className={classes.sidebar__lang} />
