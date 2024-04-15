03a2ddb95bb43eb2271d5b8f8abba094b5af9d65
Stas Guma <mcguma123@gmail.com>
2024-04-15 19:49:24 +0300

    fix Sidebar stories

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -1,11 +1,13 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { Sidebar } from './Sidebar';
+import { withStoreDecorator } from '@/shared/config/storybook/decorators/withStoreDecorator';
 
 // 👇 This default export determines where your story goes in the story list
 const meta: Meta<typeof Sidebar> = {
   title: 'Widgets/Sidebar',
   component: Sidebar,
+  decorators: [withStoreDecorator()],
 };
 
 export default meta;
