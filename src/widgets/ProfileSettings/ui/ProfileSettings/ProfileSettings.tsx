5816dc0e5376e5870e6497c41c9909ca1c544b3e
Stas Guma <mcguma123@gmail.com>
2024-04-15 18:02:21 +0300

    add the ProfileSettings widget

---
+++ src/widgets/ProfileSettings/ui/ProfileSettings/ProfileSettings.tsx
@@ -0,0 +1,42 @@
+import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { classNames } from '@/shared/lib';
+import { Loader, Typography } from '@/shared/ui';
+import { useProfileQuery } from '@/entities/Profile';
+import { UploadAvatar } from '@/features/profile/UploadAvatar';
+import { EditProfileForm } from '@/features/profile/EditProfileForm';
+
+import classes from './ProfileSettings.module.scss';
+
+interface IProps {
+  className?: string;
+}
+
+export const ProfileSettings = memo<IProps>(function ProfileSettings(props) {
+  const { className } = props;
+  const { Title } = Typography;
+  const { t } = useTranslation();
+
+  const { data, isLoading } = useProfileQuery();
+
+  return (
+    <div className={classNames(classes['profile-settings'], className)}>
+      <Title variant="3">{t('My Profile')}</Title>
+      <div className={classNames(classes['profile-settings__section'])}>
+        {isLoading
+          ? <Loader />
+          : (
+            <div className="row">
+              <div className="col-auto">
+                <UploadAvatar imageUrl={data!.avatarUrl} />
+              </div>
+              <div className="col">
+                <EditProfileForm data={data} />
+              </div>
+            </div>
+            )}
+      </div>
+    </div>
+  );
+});
