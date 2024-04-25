f5248226d0d7b6ad4083563493a3c810e56cba6a
Stas Guma <mcguma123@gmail.com>
2024-04-25 18:41:18 +0300

    update loadPath for a i18n config

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -16,6 +16,9 @@ i18n
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
+    backend: {
+      loadPath: __IS_DEV__ ? '/locales/{{lng}}/{{ns}}.json' : '../public/locales/{{lng}}/{{ns}}.json',
+    },
   });
 
 export default i18n;
